a70b5c4bd7c28fed76fdbf563d2a89d8d6e37fa1
lenyeuwaeme@gmail.com <lenyeuwaeme@gmail.com>
2023-11-06 17:24:35 -0500

    New Content

---
+++ app.js
@@ -0,0 +1,11 @@
+const instance = new Typewriter('.multitext',{
+    strings: ["Coder","Writer","Creator"],
+    autoStart: true,
+    loop: true,
+});
+
+const instance2 = new Typewriter('.Moto',{
+    strings: ["  Strength","  Integrity","  Motivation"],
+    autoStart: true,
+    loop: true,
+});
